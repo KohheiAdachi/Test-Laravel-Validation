93c2ef61ecda0f336a4d4155bac91925933ba27a
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2021-12-05 22:50:14 +0900

    task4

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,6 +9,9 @@
     {{-- @directive --}}
     {{-- {{ $message }} --}}
     {{-- @endDirective --}}
+    @error('name')
+    <span>{{ $message }}</span>
+    @enderror
     <br /><br />
     <button type="submit">Save</button>
 </form>
